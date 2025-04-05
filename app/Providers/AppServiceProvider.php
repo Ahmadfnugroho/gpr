a01a6bfb5a0235ff9d056477187bd635e6a928c7
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-06 00:24:07 +0700

    perbaikan app service pvd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,22 +7,20 @@ use App\Observers\DetailTransactionObserver;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-
-
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
-    public function boot()
+
+    public function boot(): void
     {
+        // Paksa semua URL menggunakan HTTPS di production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         DetailTransaction::observe(DetailTransactionObserver::class);
     }
-    /**
-     * Bootstrap any application services.
-     */
 }
