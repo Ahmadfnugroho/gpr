93924d4a4ad5db2f3436d7fc8f364d8440cf77ef
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-07 17:54:58 +0700

    perbaikan build-app

--- build-app.sh
+++ build-app.sh
@@ -5,9 +5,6 @@ set -e
 mkdir -p bootstrap/cache
 chmod -R 775 bootstrap/cache
 # Install dependencies
-rm -f public/storage
-ln -s ../storage/app/public public/storage
-composer require symfony/filesystem
 composer install --optimize-autoloader --no-dev
 
 # Build assets
@@ -15,7 +12,16 @@ if [ -f "package.json" ]; then
     npm install && npm run build
 fi
 
-# Optimize Laravel
+# Hapus cache lama
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+php artisan cache:clear
+
+# Buat cache baru
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+# (Opsional) Optimalkan autoloader
+composer dump-autoload
