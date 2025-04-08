2654c984750c2da250496190c4f6df3d58a72489
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-08 10:20:02 +0700

    perbaikan web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\GoogleSheetSyncController;
 use App\Http\Controllers\PdfController;
 use App\Http\Controllers\SessionController;
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
@@ -14,3 +15,6 @@ Route::post('/', [SessionController::class, 'login']);
 Route::get('pdf/{order}', PdfController::class)->name('pdf');
 Route::get('/auth/google/callback', [GoogleSheetSyncController::class, 'handleCallback'])->name('google.callback');
 Route::get('/sync', [GoogleSheetSyncController::class, 'sync'])->name('sync');
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
+});
