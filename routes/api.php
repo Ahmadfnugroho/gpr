3a9de89c48608c038f84005c7cd0785bc18ce26b
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-04-08 10:42:07 +0700

    perbaikan web.php

--- routes/api.php
+++ routes/api.php
@@ -42,10 +42,3 @@ Route::middleware('api_key')->group(function () {
 
     Route::post('/check-transaction', [TransactionController::class, 'DetailTransaction']);
 });
-Route::post('/test-upload', function (Illuminate\Http\Request $request) {
-    if ($request->hasFile('file')) {
-        $path = $request->file('file')->store('categories', 'public');
-        return response()->json(['path' => $path]);
-    }
-    return response()->json(['error' => 'No file uploaded'], 400);
-});
