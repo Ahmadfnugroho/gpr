a0c59d75e6377eaced303fbfc02d32685fd48282
Ahmad Fauzi Nugroho <ahmadkenug@gmail.com>
2025-05-22 11:18:42 +0700

    perbaikan sinkron user

--- database/migrations/2025_05_02_133551_create_product_transactions_table.php
+++ database/migrations/2025_05_02_133551_create_product_transactions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('product_transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('detail_transaction_id'); // Pastikan kolom ini ada
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
             $table->foreignId('product_item_id')->nullable()->constrained()->onDelete('set null');
